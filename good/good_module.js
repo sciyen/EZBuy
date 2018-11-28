09bdb14e1985e0944870b1e1aff07a56a7e92b27
allen <liuallen871219@gmail.com>
2018-11-28 17:26:46 +0800

    query function

--- good/good_module.js
+++ good/good_module.js
@@ -1,17 +1,31 @@
-
-module.exports.push = function (good) {
+module.exports.push = function (good,collection) {
     const config=require('./config');
     var MongoClient = require('mongodb').MongoClient;
     const url = `mongodb://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}`;
-
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("wp2018_groupA");
         var myobj =good;
-    dbo.collection("test").insertMany(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("Number of documents inserted: " + res.insertedCount);
-        db.close();
+    		dbo.collection(collection).insertMany(myobj, function(err, res) {
+        	if (err) throw err;
+        	console.log("Number of documents inserted: " + res.insertedCount);
+        	db.close();
+    		});
     });
+};
+module.exports.query=function(keyword){
+    var re=new RegExp(keyword);
+    var query={"car": re};
+    const config=require('./config');
+    var MongoClient = require('mongodb').MongoClient;
+    const url = `mongodb://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}`;
+    MongoClient.connect(url,function(err,db){
+        if(err) throw err;
+        var dbo =db.db('wp2018_groupA');
+        dbo.collection("test").find(query).toArray(function(err,result){
+            if(err) throw err;
+            console.log(result);
+            db.close();
+        });
     });
 };
