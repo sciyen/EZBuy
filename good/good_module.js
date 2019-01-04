d554d76e9f430add581f50a9eba49964548800a6
allen <liuallen871219@gmail.com>
2019-01-04 13:45:03 +0800

    add update_item_info in good_module.js

--- good/good_module.js
+++ good/good_module.js
@@ -93,34 +93,73 @@ module.exports.removeAll=function(collection){
           if(err) console.log('Remove Error, ', err);})
     });
 };
-module.exports.update_item_info=function(collection){
+/*module.exports.update_item_info=function(collection){
     const config=require('./config');
     var MongoClient = require('mongodb').MongoClient;
     const url = `mongodb://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}`;
     MongoClient.connect(url,function(err,db){
         if(err) throw err;
         var dbo =db.db('wp2018_groupA');
+        var temp={};
+        //var item= dbo.collection(collection).find({}).toArray(function(err,result,temp){temp=result;});
+        //console.log(temp);
         dbo.collection(collection).find({}).toArray(function(err,result){
             if(err) throw err;
-            console.log(result);
+            //console.log(result);
             for(var i=0;i<result.length;++i){
-              //var reg= new RegExp(result.item);
-              var good=db.collection("EZBuyGoods").find({"message":{$regex:result[i].item}}).toArray();
-              for(var j=0;j<good.length;++j){
-                if(result[i].posts.map(function(e){return e.post_id}).indexof(good[j].id)==-1){
-                  //var obj={"post_id"=good[j].id,"post_time"=good[j].update_time};
-                  var obj={};
-                  obj.post_id=good[j].id;
-                  obj.post_time=good[j].update_time;
-                  result[i].posts.push(obj);
-                  result[i].last_update_time=Date.now();
-                  db.collection(collection).update({"item":result[i].item}
-                      ,{"item":result[i].item,"posts":result[i].posts,"last_update_time":result[i].last_update_time});
-                }
-              }
-            }
+              var item_info= result[i].posts.map(function(e){return e.post_id;});
+              var temp=result[i];
+              //console.log(temp.item);
+              dbo.collection("EZBuyGoods").find({"message":{$regex:result[i].item}}).toArray(function(err,good){
+                //console.log(good);
+                for(var j=0;j<good.length;++j){
+                  if(item_info.indexOf(good[j].id)==-1){
+                    var obj={};
+                    obj.post_id=good[j].id;
+                    obj.post_time=good[j].updated_time;;
+                    temp.posts.push(obj);
+                    temp.last_update_time=Date.now();
+                //    console.log(temp);
+                    //dbo.collection(collection).findOneAndDelete({"item":temp.item},function(err,res){});
+                    //dbo.collection(collection).insert(temp,function(err,res){})
 
+                  }
+                }
+                //console.log(temp.item);
+               // dbo.collection(collection).findOneAndDelete({"item":temp.item},function(err,res){});
+               // dbo.collection(collection).insert(temp,function(err,res){});i 
+              }) 
+            }
             db.close();
         });
     });
 };
+*/
+module.exports.update_item_info=function(collection,keyword){
+    const config=require('./config');
+    var MongoClient = require('mongodb').MongoClient;
+    var item_info= keyword.posts.map(function(e){return e.post_id;});
+    const url = `mongodb://${config.mongodb.user}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}`;
+    MongoClient.connect(url,function(err,db){
+        if(err) throw err;
+        var dbo=db.db("wp2018_groupA");
+        dbo.collection("EZBuyGoods").find({"message":{$regex:keyword.item}}).toArray(function(err,match){
+            for(var j=0;j<match.length;++j){
+              if(item_info.indexOf(match[j].id)==-1){
+                var obj={};
+                obj.post_id=match[j].id;
+                obj.post_time=match[j].updated_time;
+                console.log(obj);
+                keyword.posts.push(obj);
+                keyword.last_update_time=Date.now();
+              }
+            }
+            console.log(keyword);
+            dbo.collection(collection).updateOne({"item":keyword.item},keyword);
+            db.close();
+          });
+           //console.log(keyword);
+           //dbo.collection(collection).updateOne({"item":keyword.item},keyword);
+    });
+};
+
